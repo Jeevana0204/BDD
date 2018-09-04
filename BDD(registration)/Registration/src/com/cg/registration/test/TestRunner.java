42279628e239016fbdd510783d327993a9915f9c
Jeevana0204 <41135713+Jeevana0204@users.noreply.github.com>
2018-09-04 17:35:08 +0530

    Add files via upload

---
+++ BDD(registration)/Registration/src/com/cg/registration/test/TestRunner.java
@@ -0,0 +1,16 @@
+package com.cg.registration.test;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features= {"feature"},
+		glue= {"com.cg.registration.stepdefinition"},
+		tags= {"@module3"}
+)
+public class TestRunner {
+
+}
